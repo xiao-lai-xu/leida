09f23a18153facf994fffc24b216640c7687ce70
xiao-lai-xu <1452201361@qq.com>
2025-04-07 19:56:25 +0800

    11

--- stl/mod_squared_unit.v
+++ stl/mod_squared_unit.v
@@ -1,37 +1,38 @@
 module mod_squared_unit(
-    input clk,
-    input reset,
-    input [7:0] x,
-    input [7:0] y,
+    input logic signed [15:0] x, //实部
+    input logic signed [15:0] y, //虚部
     input [7:0] conf_bit_mask,
+
     
-    output reg [15:0] r
+    output reg [31:0] r
 );
-    wire [15:0] mul_r;
+
+
+//--------------------近似乘法器------------------------------------------
     wire [15:0] mul_r1;
-    
-    fixed_int_mul #(.WIDTH(8), .DEC_POINT_POS(4)) m_fixed_int_mul(
+    wire [15:0] mul_r2;    
+
+    fixed_int_mul #(.WIDTH(8), .DEC_POINT_POS(4)) m_fixed_int_mul_x(
         .A(x),
+        .B(x),
+        .Conf_Bit_Mask(conf_bit_mask),
+        .R(mul_r1)
+    );
+
+    fixed_int_mul #(.WIDTH(8), .DEC_POINT_POS(4)) m_fixed_int_mul_y(
+        .A(y),
         .B(y),
         .Conf_Bit_Mask(conf_bit_mask),
-        .R(mul_r)
+        .R(mul_r2)
     );
+
+    assign r = mul_r1 + mul_r2; //实部平方加虚部平方
     
-    bit_mask_sel #(.WIDTH(16)) m_bit_mask_sel(
-        .sel(conf_bit_mask[1:0]),
-        .x(mul_r),
-        .y(16'b0),
-        .r(mul_r1)
-    );
-    
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            r <= 16'b0;
-        end else begin
-            r <= mul_r1;
-        end
-    end
-);
+//----------------------普通乘法-----------------------------------------------
+    logic signed [31:0] a2 = x*x;
+    logic signed [31:0] b2 = y*y;
+    assign r = a2 + b2; //实部平方加虚部平方
+
 
 
 
